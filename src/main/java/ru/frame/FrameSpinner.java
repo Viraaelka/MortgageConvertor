8cc418b4ef93c57cc1db361bdc962d84093a8500
viraaelka <aelya089@gmail.com>
2019-03-04 15:28:42 +0300

    additional calculator with Jspinner

--- src/main/java/ru/frame/FrameSpinner.java
+++ src/main/java/ru/frame/FrameSpinner.java
@@ -1,21 +1,32 @@
 package ru.frame;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
 
-public class FrameClass {
+public class FrameSpinner {
+
     JFrame frame;
-    public FrameClass(){
+    public FrameSpinner(){
         frame = new JFrame();
         frame.setTitle("MortageConvertor");
+/*
+        for(int i = 0; i < 4; i++){
+            jtoggle[i] = new JToggleButton("i: ");
+            jtoggle[i].setBounds(10+i, 10+i, 10,10);
+            frame.add(jtoggle[i]);
+
+        }
+*/
 
         interestRate.setBounds(70, 20, 220, 20);
         labelValue.setBounds(30, 60, 220, 20);
         textValue.setBounds(30,90,220,20);
         labelDownPayment.setBounds(30, 120, 220, 20);
-        downPayment.setBounds(30,150, 220, 20);
+        slidedownPayment.setBounds(30,150, 220, 20);
         labelTerm.setBounds(30, 180, 220, 20);
-        term.setBounds(30, 210, 220, 20);
+        slideterm.setBounds(30, 210, 220, 20);
         labelLoan.setBounds(30, 240, 220,20);
         panel.setBounds(30, 270, 220, 250);
 
@@ -24,12 +35,17 @@ public class FrameClass {
         labelMonthlyPayment.setBounds(30,360,220,100);
         labelRate.setBounds(30,360,220,100);
 
+        textValue.setPaintTicks(true);
+        textValue.setMajorTickSpacing(20);
+        textValue.setMinorTickSpacing(5);
+
+
         panel.setBackground(Color.LIGHT_GRAY);
         panel.setLayout(null);
 
         frame.add(textValue);
-        frame.add(downPayment);
-        frame.add(term);
+        frame.add(slidedownPayment);
+        frame.add(slideterm);
         frame.add(interestRate);
         frame.add(labelValue);
         frame.add(labelDownPayment);
@@ -46,7 +62,6 @@ public class FrameClass {
         panel.add(labelRate);
         panel.add(valueRate);
 
-
         panel.getComponent(0).setBounds(10, 10, 220, 30);
         for(int i = 1; i <= 6; i++){
             panel.getComponent(i).setBounds(10, 10*i + 20*(i-1), 220,100);
@@ -54,26 +69,26 @@ public class FrameClass {
 
         frame.add(panel);
 
+        listener = new ChangeListener() {
+            public void stateChanged(ChangeEvent e) {
+                textValue = (JSlider)e.getSource();
+                labelRate.setText(" " + textValue.getValue());
+            }
+        };
+
         frame.setSize(300, 600);
         frame.setLayout(null);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-
     }
     String[]massive = new String[]{"Альфа", "Тинькофф", "Сбербанк", "ВТБ"};
     //  JComboBox<String> comboBox = new JComboBox<>(massive);
-    JLabel interestRate = new JLabel("Калькулятор кредитования");
+    JLabel interestRate = new JLabel("Welcome to this app");
 
     JLabel labelValue = new JLabel("Стоимость недвижимости");
     JLabel labelDownPayment = new JLabel("Первоначальный взнос");
     JLabel labelTerm = new JLabel("Срок кредитования");
-
-
-    JTextField textValue = new JTextField();
-    JTextField downPayment = new JTextField();
-    JTextField term = new JTextField();
-
 
     JTextField loan = new JTextField("");
     JTextField monthlyPayment = new JTextField();
@@ -87,4 +102,9 @@ public class FrameClass {
     JLabel valueMonthlyPayment = new JLabel("test");
     JLabel labelRate = new JLabel("Процентная ставка");
     JLabel valueRate = new JLabel("test");
+
+    JSlider textValue = new JSlider(0,1000,1);
+    JSlider slidedownPayment = new JSlider();
+    JSlider slideterm = new JSlider();
+    ChangeListener listener;
 }
