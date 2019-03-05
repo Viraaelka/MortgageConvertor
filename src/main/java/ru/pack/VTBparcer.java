9a75e83e5eaf4227c3e8f5c9bd499b9d40072ba6
viraaelka <aelya089@gmail.com>
2019-03-05 19:10:18 +0300

    VTBParcer is done

--- src/main/java/ru/pack/VTBparcer.java
+++ src/main/java/ru/pack/VTBparcer.java
@@ -1,6 +1,25 @@
 package ru.pack;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+import java.net.URL;
+
 public class VTBparcer {
-    String linkBuilded = "https://www.vtb.ru/personal/ipoteka/novostrojki/#calc_0#";
-    String linkInProcessBuinding = "https://www.vtb.ru/personal/ipoteka/vtorichnoe-zhile/#calc_0#";
+    static String linkBuilded = "https://www.vtb.ru/personal/ipoteka/novostrojki/";
+    static String linkInProcessBuinding = "https://www.vtb.ru/personal/ipoteka/vtorichnoe-zhile/#calc_0#";
+
+    public double getVTBRate()throws IOException{
+        Document document = Jsoup.parse(new URL(linkBuilded), 3000);
+        Element elem = document.select("div [class = media-slider__inner clear-after]").first();
+        Element tdElem = elem.select("li").first();
+        String kup = tdElem.text();
+        kup = getPattern(kup);
+        return 0.0;
+    }
+    public String getPattern(String str){
+        return null;
+    }
 }
