2ed966da95573a0429bcca4ee96f1e008f456c03
viraaelka <aelya089@gmail.com>
2019-03-05 17:40:35 +0300

    Tinkoff is done

--- src/main/java/ru/pack/TinkoffParcer.java
+++ src/main/java/ru/pack/TinkoffParcer.java
@@ -1,6 +1,22 @@
 package ru.pack;
 
+import org.jsoup.Jsoup;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+import java.net.URL;
+
 public class TinkoffParcer {
-    String link = "https://www.tinkoff.ru/loans/mortgage/";
+    public static String link = "https://www.tinkoff.ru/loans/mortgage/";
+
+    public double getTinkoffRate() throws IOException{
+        Document doc = Jsoup.parse(new URL(TinkoffParcer.link), 3000);
+        Element elem = doc.select("td[class = ui-table-properties__table-cell ui-table-properties__table-cell_body]").get(6);
+        String k = elem.text().replace(",", ".");
+        k = k.substring(0, k.length()-1);
+        return Double.parseDouble(k);
+    }
 
 }
